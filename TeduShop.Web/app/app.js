e2586be02d40432becf80415a18297fd72dae461
thanglamduy <thanglamduy@gmail.com>
2018-10-22 20:31:08 +0700

    Routing app uisng the template admin page

---
+++ TeduShop.Web/app/app.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+(function () {
+    angular.module('tedushop', ['tedushop.products', 'tedushop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider']; //regist this state to this config
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
